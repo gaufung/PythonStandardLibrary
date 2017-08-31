a5a32ed2a98a8bc3672789a52047b23509ffa747
gaufung <gaufung@outlook.com>
2017-08-31 14:59:39 +0800

    add text part

---
+++ Text/TextWrapper/textwrap_example.py
@@ -0,0 +1,6 @@
+sample_text='''
+    The textwrap module can be used to format text four output in
+    situations where pretty-printing is desired. It offers
+    programmatic functionality simmilar to the paragraph wrapping
+    or filling features found in many text edits
+    '''
