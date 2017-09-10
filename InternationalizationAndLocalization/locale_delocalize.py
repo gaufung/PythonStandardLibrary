639da2abfcfc70cadbfc7c8835fcd814ca449daa
gaufung <gaufung@outlook.com>
2017-09-10 08:49:10 +0800

    localization

---
+++ InternationalizationAndLocalization/locale_delocalize.py
@@ -0,0 +1,22 @@
+import locale
+
+import locale
+
+sample_locales = [
+    ('USA', 'en_US'),
+    ('France', 'fr_FR'),
+    ('Spain', 'es_ES'),
+    ('Portugal', 'pt_PT'),
+    ('Poland', 'pl_PL'),
+    ('China', 'zh_CN')
+]
+
+for name, loc in sample_locales:
+    locale.setlocale(locale.LC_ALL, loc)
+    localized = locale.format('%0.2f', 123456.78, grouping=True)
+    delocalized = locale.delocalize(localized)
+    print('{:>10}: {:>10}  {:>10}'.format(
+        name,
+        localized,
+        delocalized,
+    ))
